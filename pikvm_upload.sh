793287aba83fe17afb8a0996f1ffebd921ee873a
Brendan Jackman <jackmanb@google.com>
2024-11-25 16:47:22 +0000

    Revert "Use HTTP/1.1 to workaround weird upload error"
    This reverts commit ab5d1a95a6dc1d1a09995413acd8ecc35d74a7ab.
    
    Doesn't fix it. I broke the PI trying to fix this lol, had to reflash
    from scratch.

--- pikvm_upload.sh
+++ pikvm_upload.sh
@@ -7,8 +7,7 @@ curl -v -X POST  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.
 # Delete existing image.
 curl -v -X POST  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/remove?image=image.raw"
 # Compress image and upload it.
-# --http1.1: https://stackoverflow.com/questions/56413290/getting-curl-92-http-2-stream-1-was-not-closed-cleanly-internal-error-err
-gzip --stdout image.raw |  curl -v -X POST --data-binary @- -H 'Content-Encoding: gzip'  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/write?image=image.raw" --http1.1
+gzip --stdout image.raw |  curl -v -X POST --data-binary @- -H 'Content-Encoding: gzip'  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/write?image=image.raw"
 # Select image for MSD
 curl -v -X POST  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/set_params?image=image.raw&cdrom=0"
 # Reconnect MSD
