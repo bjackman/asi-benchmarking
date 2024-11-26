8b41052b776f50344ed3e4f9b3b7c26b3a055cfd
Brendan Jackman <jackmanb@google.com>
2024-11-26 12:31:52 +0000

    Make MSD rw

--- pikvm_upload.sh
+++ pikvm_upload.sh
@@ -8,7 +8,7 @@ curl -v -X POST  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.
 curl -v -X POST  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/remove?image=image.raw"
 # Compress image and upload it.
 gzip --stdout image.raw |  curl -v -X POST --data-binary @- -H 'Content-Encoding: gzip'  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/write?image=image.raw"
-# Select image for MSD
-curl -v -X POST  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/set_params?image=image.raw&cdrom=0"
+# Select image & params for MSD.
+curl -v -X POST  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/set_params?image=image.raw&cdrom=0&rw=1"
 # Reconnect MSD
 curl -v -X POST  -k -H X-KVMD-User:admin -H X-KVMD-Passwd:$PASSWORD "https://34.90.180.154:8080/api/msd/set_connected?connected=1"
