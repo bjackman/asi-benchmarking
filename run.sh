f5678cfae1135d7a62766bc0052026462092c89d
Brendan Jackman <jackmanb@google.com>
2024-11-27 12:49:47 +0000

    Messing around with powering on/off host

--- run.sh
+++ run.sh
@@ -19,15 +19,23 @@ ssh_pikvm() {
     ssh -p "$PIKVM_SSH_PORT" "$PIKVM_SSH_USER@$PIKVM_HOST" "$@"
 }
 
+host_ssh_visible() {
+    nc -zw5 "$HOST" $HOST_SSH_PORT
+}
+
 # Shut down host so it doesn't leak proxy connections, unless it seems to be off already.
 if [[ $(curl_pikvm GET /api/atx | jq ".result.leds.power") != "false" ]]; then
-    ansible-playbook -i host-inventory.yaml host-shutdown.yaml
+    if host_ssh_visible; then
+        ansible-playbook -i host-inventory.yaml host-shutdown.yaml || echo "shutdown failed, continuing"
+    fi
+
+    # Kill the power. It's pretty likely it's still shutting down (or shutdown
+    # failed), but that's fine since we're about to totally replace the disk
+    # anyway.  The main reason we shut it down was to kill the SSH tunneling
+    # service which is probably done by now.
+    # For some reason I found /api/atx/power?action=off doesn't do anything at least for my HW.
+    curl_pikvm POST "/api/atx/click?button=power&wait=1"
 fi
-# Kill the power. It's pretty likely it's still shutting down (or shutdown
-# failed), but that's fine since we're about to totally replace the disk anyway.
-# The main reason we shut it down was to kill the SSH tunneling service which is
-# probably done by now.
-curl_pikvm POST "/api/atx/power?action=on"
 
 # Disconnect mass storage, so we can write it.
 curl_pikvm POST "/api/msd/set_connected?connected=0"
@@ -51,6 +59,20 @@ curl_pikvm POST "/api/msd/set_connected?connected=1"
 
 # Boot 'er up
 curl_pikvm POST "/api/atx/power?action=on"
+
+# Boot 'er up
+curl_pikvm POST "/api/atx/power?action=on"
+# Wait until the SSH port becomes visible.
+# -z = scan only,  -w5 = 5s timeout
+deadline_s=$(($(date +%s) + 120))
+while ! host_ssh_visible; do
+    current_time_s=$(date +%s)
+    if (( current_time_s > deadline_s )); then
+        echo "Timed out after 2m waiting for host SSH port to appear"
+        exit 1
+    fi
+    sleep 1
+done
 
 # Run the benchmark
 ansible-playbook -i host-inventory.yaml host-setup.yaml
